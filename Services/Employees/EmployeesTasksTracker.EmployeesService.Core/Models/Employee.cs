e99daa1a289636ed884d2129c805aab3098a580c
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-14 14:43:13 +0400

    Created entity for Employee and enum for his role.

---
+++ Services/Employees/EmployeesTasksTracker.EmployeesService.Core/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesTasksTracker.EmployeesService.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesTasksTracker.EmployeesService.Core.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string UserName { get; set; }
+        public Role Role { get; set; }
+    }
+}
