e981e84480ec641121f6e560a65e78d0a1933344
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-15 15:17:26 +0400

    Added generic extension method for adding and seeding databases.

---
+++ Common/Shared/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Shared.Extensions
+{
+    public static class DatabaseExtensions
+    {
+
+        public static void AddDatabaseService<TContext>(this IServiceCollection services, IConfiguration configuration, string connectionString = "DefaultConnection")
+            where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString(connectionString)));
+        }
+
+        public static async Task InitializeDatabaseAsync<TContext>(this IServiceProvider serviceProvider, Func<TContext, Task>? initializer = null)
+            where TContext : DbContext
+        {
+            using var score = serviceProvider.CreateScope();
+            var services = score.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<TContext>();
+
+                if (initializer != null)
+                {
+                    await initializer(context);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Database initialization failure: {ex.Message}");
+            }
+        }
+    }
+}
