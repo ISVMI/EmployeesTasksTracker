1f3547080b75f1624f422684f94de2dc892fda8a
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-15 13:01:46 +0400

    Created db context for Employees and added default connection string to the appsettings.json

---
+++ Services/Employees/EmployeesTasksTracker.EmployeesService.Infrastructure/Repositories/EmployeesRepo.cs
@@ -0,0 +1,41 @@
+﻿using EmployeesTasksTracker.EmployeesService.Core.Interfaces;
+using EmployeesTasksTracker.EmployeesService.Core.Models;
+using EmployeesTasksTracker.EmployeesService.Infrastructure.Data;
+
+namespace EmployeesTasksTracker.EmployeesService.Infrastructure.Repositories
+{
+    internal class EmployeesRepo : IEmployeesRepo
+    {
+        private readonly EmployeesContext _context;
+
+        public EmployeesRepo(EmployeesContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Guid> CreateAsync(Employee entity, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Guid id, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> GetAllAsync(CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetByIdAsync(Guid id, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateAsync(Employee entity, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
