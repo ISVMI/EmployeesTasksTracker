cf35ebf148a9c4844c5f901f9b28cf3195cac75e
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-16 13:01:54 +0400

    Added extension methods for services registration.

---
+++ Services/Employees/EmployeesTasksTracker.EmployeesService.Application/Extensions/ServicesRegistration.cs
@@ -0,0 +1,30 @@
+﻿using EmployeesTasksTracker.EmployeesService.Application.Handlers;
+using EmployeesTasksTracker.EmployeesService.Application.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace EmployeesTasksTracker.EmployeesService.Application.Extensions
+{
+    public static class ServicesRegistration
+    {
+
+        public static IServiceCollection AddApplication(this IServiceCollection services) 
+        {
+            services.AddAutoMapper(cfg => cfg.AddProfile<EmployeesProfile>(), Assembly.GetExecutingAssembly());
+
+            var assemblies = new Assembly[]
+            {
+                Assembly.GetExecutingAssembly(),
+                typeof(CreateEmployeeHandler).Assembly,
+                typeof(EditEmployeeHandler).Assembly,
+                typeof(DeleteEmployeeHandler).Assembly,
+                typeof(GetAllEmployeesHandler).Assembly,
+                typeof(GetEmployeeByIdHandler).Assembly,
+            };
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+
+            return services;
+        }
+    }
+}
---
+++ Services/Employees/EmployeesTasksTracker.EmployeesService.Infrastructure/Extensions/InfrastructureRegistration.cs
@@ -0,0 +1,17 @@
+﻿using EmployeesTasksTracker.EmployeesService.Core.Interfaces;
+using EmployeesTasksTracker.EmployeesService.Infrastructure.Data;
+using EmployeesTasksTracker.EmployeesService.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeesTasksTracker.EmployeesService.Infrastructure.Extensions
+{
+    public static class InfrastructureRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDatabaseService<EmployeesContext>(configuration);
+            services.AddScoped<IEmployeesRepo, EmployeesRepo>();
+        }
+    }
+}
