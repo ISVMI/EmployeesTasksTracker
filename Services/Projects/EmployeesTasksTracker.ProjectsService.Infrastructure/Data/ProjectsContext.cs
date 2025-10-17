b03949852c6028cba9799fb08c7fa590cce0d9a3
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-17 12:47:36 +0400

    Added db context.

---
+++ Services/Projects/EmployeesTasksTracker.ProjectsService.Infrastructure/Data/ProjectsContext.cs
@@ -0,0 +1,47 @@
+﻿using EmployeesTasksTracker.ProjectsService.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesTasksTracker.ProjectsService.Infrastructure.Data
+{
+    public class ProjectsContext : DbContext
+    {
+        public ProjectsContext(DbContextOptions<ProjectsContext> options) : base(options) { }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<ProjectsEmployees> ProjectsEmployees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Project>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Project>()
+                .Property(p => p.Name)
+                .HasColumnName("Name");
+
+            modelBuilder.Entity<Project>()
+                .Property(p => p.Description)
+                .HasColumnName("Description");
+
+            modelBuilder.Entity<Project>()
+                .Property(p => p.Supervisor)
+                .HasColumnName("Supervisor");
+
+            modelBuilder.Entity<Project>()
+                .Property(p => p.Manager)
+                .HasColumnName("Manager");
+
+            modelBuilder.Entity<ProjectsEmployees>()
+                .HasKey(pe => new { pe.ProjectId, pe.EmployeeId });
+
+            modelBuilder.Entity<ProjectsEmployees>()
+                .Property(pe => pe.IsSupervisor)
+                .HasColumnName("IsSupervisor");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
