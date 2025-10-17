13bc9f56ad00cfecd24a417b49567a51ae4dc57b
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-17 12:25:56 +0400

    Added main entities.

---
+++ Services/Projects/EmployeesTasksTracker.ProjectsService.Core/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesTasksTracker.ProjectsService.Core.Models
+{
+    public class Project
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid Supervisor { get; set; }
+        public Guid Manager { get; set; }
+
+    }
+}
