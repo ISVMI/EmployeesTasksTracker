5696968255de7f8361c25098153fe71b722b81fe
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-18 11:04:36 +0400

    Added commands, queries and DTOs

---
+++ Services/Projects/EmployeesTasksTracker.ProjectsService.Application/DTOs/EditProjectDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeesTasksTracker.ProjectsService.Application.DTOs
+{
+    public record EditProjectDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public Guid Supervisor { get; init; }
+        public Guid Manager { get; init; }
+    }
+}
---
+++ Services/Projects/EmployeesTasksTracker.ProjectsService.Application/DTOs/ProjectDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeesTasksTracker.ProjectsService.Application.DTOs
+{
+    public record ProjectDTO
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public Guid Supervisor { get; init; }
+        public Guid Manager { get; init; }
+    }
+}
