d9d81277614c01aaad79d69fdfc234e527a03578
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-18 11:29:31 +0400

    Added mapping profile and handlers.

---
+++ Services/Projects/EmployeesTasksTracker.ProjectsService.Application/Handlers/EditProjectHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using EmployeesTasksTracker.ProjectsService.Application.Commands;
+using EmployeesTasksTracker.ProjectsService.Application.DTOs;
+using EmployeesTasksTracker.ProjectsService.Core.Interfaces;
+using MediatR;
+
+namespace EmployeesTasksTracker.ProjectsService.Application.Handlers
+{
+    public class EditProjectHandler : IRequestHandler<EditProjectCommand, ProjectDTO>
+    {
+        private readonly IProjectsRepo _repo;
+        private readonly IMapper _mapper;
+
+        public EditProjectHandler(IProjectsRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public async Task<ProjectDTO> Handle(EditProjectCommand request, CancellationToken cancellationToken)
+        {
+            var projectToEdit = await _repo.GetByIdAsync(request.ProjectToEdit.Id, cancellationToken);
+            await _repo.UpdateAsync(projectToEdit, cancellationToken);
+            return _mapper.Map<ProjectDTO>(projectToEdit);
+        }
+    }
+}
---
+++ Services/Projects/EmployeesTasksTracker.ProjectsService.Application/Handlers/GetAllProjectsHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using EmployeesTasksTracker.ProjectsService.Application.DTOs;
+using EmployeesTasksTracker.ProjectsService.Application.Queries;
+using EmployeesTasksTracker.ProjectsService.Core.Interfaces;
+using MediatR;
+
+namespace EmployeesTasksTracker.ProjectsService.Application.Handlers
+{
+    public class GetAllProjectsHandler : IRequestHandler<GetAllProjectsQuery, IEnumerable<ProjectDTO>>
+    {
+        private readonly IProjectsRepo _repo;
+        private readonly IMapper _mapper;
+
+        public GetAllProjectsHandler(IProjectsRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProjectDTO>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
+        {
+            var projects = await _repo.GetAllAsync(cancellationToken);
+            return _mapper.Map<IEnumerable<ProjectDTO>>(projects);
+        }
+    }
+}
