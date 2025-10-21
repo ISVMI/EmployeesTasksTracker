8f1b291015be79b04b7ab687a031e7b3653336cc
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-21 11:52:34 +0400

    Added task group model and configured context

---
+++ Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Infrastructure/Data/TaskGroupsContext.cs
@@ -0,0 +1,25 @@
+﻿using EmployeesTasksTracker.TasksGroupsService.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Data
+{
+    public class TaskGroupsContext : DbContext
+    {
+        public TaskGroupsContext(DbContextOptions<TaskGroupsContext> options) : base(options) { }
+
+        DbSet<TaskGroup> TaskGroups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<TaskGroup>()
+                .HasKey(tg => tg.Id);
+
+            modelBuilder.Entity<TaskGroup>()
+                .Property(tg => tg.Name)
+                .HasColumnName("Name");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
