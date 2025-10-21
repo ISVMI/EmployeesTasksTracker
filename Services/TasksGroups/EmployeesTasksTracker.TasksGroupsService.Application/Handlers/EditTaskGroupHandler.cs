53e16a9338e1a7e9e6e5237806ea1893d34e6b23
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-21 13:10:37 +0400

    Changed edit task group handler in order for it to work.

--- Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Application/Handlers/EditTaskGroupHandler.cs
+++ Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Application/Handlers/EditTaskGroupHandler.cs
@@ -2,6 +2,7 @@
 using EmployeesTasksTracker.TasksGroupsService.Application.Commands;
 using EmployeesTasksTracker.TasksGroupsService.Application.DTOs;
 using EmployeesTasksTracker.TasksGroupsService.Core.Interfaces;
+using EmployeesTasksTracker.TasksGroupsService.Core.Models;
 using MediatR;
 
 namespace EmployeesTasksTracker.TasksGroupsService.Application.Handlers
@@ -19,7 +20,7 @@ namespace EmployeesTasksTracker.TasksGroupsService.Application.Handlers
 
         public async Task<TaskGroupDto> Handle(EditTaskGroupCommand request, CancellationToken cancellationToken)
         {
-            var taskGroupToEdit = await _repo.GetByIdAsync(request.TaskGRoupToEdit.Id, cancellationToken);
+            var taskGroupToEdit = _mapper.Map<TaskGroup>(request.TaskGRoupToEdit);
             await _repo.UpdateAsync(taskGroupToEdit,cancellationToken);
             return _mapper.Map<TaskGroupDto>(taskGroupToEdit);
         }
