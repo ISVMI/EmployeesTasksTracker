16ea749d584eea980d2c7db6c960d2b6b3354b1b
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-24 13:00:55 +0400

    Implemented generic method for generating db context options builder for db context factory to use.

---
+++ Common/Shared/Extensions/ContextFactoryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Shared.Extensions
+{
+    public static class ContextFactoryExtensions
+    {
+        public static DbContextOptionsBuilder<TContext> GetOptionsBuilder<TContext>()
+            where TContext : DbContext
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TContext>();
+
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+            var path = Directory.GetCurrentDirectory()[..^14] + "Api";
+
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(path)
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+
+            return optionsBuilder;
+        }
+    }
+}
