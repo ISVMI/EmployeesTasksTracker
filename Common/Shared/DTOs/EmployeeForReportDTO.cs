207cb07bc18fadf9149ab6549dd01afa65f3fb4b
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-24 18:26:13 +0400

    Added DTOs for pdf reports generation.

---
+++ Common/Shared/DTOs/EmployeeForReportDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.DTOs
+{
+    public record EmployeeForReportDTO
+    {
+        public string Name { get; init; }
+        public string Surname { get; init; }
+        public string Patronymic { get; init; }
+        public string Role { get; init; }
+    }
+}
---
+++ Common/Shared/DTOs/TaskForReportDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.DTOs
+{
+    public record TaskForReportDTO
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string Deadline { get; init; }
+        public string CreatedAt { get; init; }
+        public string Status { get; init; }
+        public string Priority { get; init; }
+    }
+}
