0b8a00e722ce0981357a3fb54688e5aa16e0bbde
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-25 15:04:17 +0400

    Added and implemented http client. Also added interfaces for pdf reports generation (not implemented yet).

---
+++ Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Infrastructure/Clients/ProjectsClient.cs
@@ -0,0 +1,23 @@
+﻿using EmployeesTasksTracker.TasksGroupsService.Application.Interfaces;
+
+namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Clients
+{
+    public class ProjectsClient : IProjectsClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProjectsClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetProjectName(Guid id, CancellationToken cancellationToken = default)
+        {
+            var responce = await _httpClient.GetAsync($"api/Projects/{id}?nameRequested=true", cancellationToken);
+
+            responce.EnsureSuccessStatusCode();
+
+            return await responce.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Infrastructure/Clients/TasksClient.cs
@@ -0,0 +1,34 @@
+﻿using EmployeesTasksTracker.TasksGroupsService.Application.Interfaces;
+using Shared.DTOs;
+using System.Net.Http.Json;
+
+namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Clients
+{
+    public class TasksClient : ITasksClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public TasksClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Guid> GetProjectId(Guid tasksGroupId, CancellationToken cancellationToken = default)
+        {
+            var responce = await _httpClient.GetAsync($"api/Tasks/GetProjectId/{tasksGroupId}", cancellationToken);
+
+            responce.EnsureSuccessStatusCode();
+
+            return await responce.Content.ReadFromJsonAsync<Guid>();
+        }
+
+        public async Task<IEnumerable<TaskForReportDTO>> GetTasks(Guid tasksGroupId, CancellationToken cancellationToken = default)
+        {
+            var responce = await _httpClient.GetAsync($"api/Tasks/GetTasksByGroupId/{tasksGroupId}", cancellationToken);
+
+            responce.EnsureSuccessStatusCode();
+
+            return await responce.Content.ReadFromJsonAsync<IEnumerable<TaskForReportDTO>>();
+        }
+    }
+}
