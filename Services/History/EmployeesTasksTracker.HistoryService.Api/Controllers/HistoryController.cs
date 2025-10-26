90530c35c7e36021bfca65049a575d90655a349b
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-26 14:59:41 +0400

    Added service for task changes history tracking.

---
+++ Services/History/EmployeesTasksTracker.HistoryService.Api/Controllers/HistoryController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesTasksTracker.HistoryService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HistoryController : ControllerBase
+    {
+        
+    }
+}
