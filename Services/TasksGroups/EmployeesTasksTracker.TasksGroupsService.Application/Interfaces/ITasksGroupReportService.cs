39d7199b780f1118ca2536572d2e4c10faeac3b2
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-26 13:40:55 +0400

    Implemented method for getting report data.

---
+++ Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Application/Interfaces/ITasksGroupReportService.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Models;
+
+namespace EmployeesTasksTracker.TasksGroupsService.Application.Interfaces
+{
+    public interface ITasksGroupReportService
+    {
+        Task<TasksGroupReportModel> GetTasksGroupReportDataAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
