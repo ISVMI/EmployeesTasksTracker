ccc0a7e7a9206642ddc7a1120640c145d6b4f822
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-26 11:44:17 +0400

    Added model for tasks groups report.

---
+++ Common/Shared/Models/TasksGroupReportModel.cs
@@ -0,0 +1,13 @@
+﻿using Shared.DTOs;
+
+namespace Shared.Models
+{
+    public class TasksGroupReportModel
+    {
+        public string ReportTitle { get; set; }
+        public DateTime GeneratedAt { get; set; } = DateTime.Now;
+        public string Name { get; set; }
+        public string ProjectName { get; set; }
+        public List<TaskForReportDTO> Tasks { get; set; } = new();
+    }
+}
