7faaa21c2635f2ce5354b3b8415cd8e651ef2e34
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-27 17:02:46 +0400

    Implemented message consumers and SignalR

---
+++ Services/Notification/EmployeesTasksTracker.NotificationService.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Text.Json;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5240/notifications")
+    .Build();
+
+connection.On<object>("ReceiveNotification", msg =>
+{
+    Console.WriteLine("Notification recieved: " + JsonSerializer.Serialize(msg));
+});
+
+await connection.StartAsync();
+
+Console.WriteLine("Connected to notification hub");
+
+Console.ReadLine();
---
+++ Services/Notification/EmployeesTasksTracker.NotificationService/Hubs/NotificationHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace EmployeesTasksTracker.NotificationService.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Client connected {Context.ConnectionId}");
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            Console.WriteLine($"Client disconnected {Context.ConnectionId}");
+
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
