975e917c20cd1349e50487e237ac951cdd37fe5e
dasdsdas sdaasd <skorliak@mail.ru>
2025-10-27 17:18:58 +0400

    Added messages for notifications

---
+++ Common/Shared/Messages/EmployeeAssigned.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Shared.Messages
+{
+    public record EmployeeAssigned
+    {
+        public Guid TaskId { get; set; }
+        public string TaskName { get; set; }
+        public Guid EmployeeId { get; set; }
+
+    }
+}
---
+++ Common/Shared/Messages/TaskStatusChanged.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Shared.Messages
+{
+    public record TaskStatusChanged
+    {
+        public Guid TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string OldStatus { get; set; }
+        public string NewStatus { get; set; }
+    }
+}
