54b3f7329b02d25b7148852cc05f230fa1bb0125
dasdsdas sdaasd <skorliak@mail.ru>
2025-11-19 14:43:54 +0400

    Changed exception throwing and catching in order to use global exception handler.

--- Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Infrastructure/Repositories/TasksGroupsRepo.cs
+++ Services/TasksGroups/EmployeesTasksTracker.TasksGroupsService.Infrastructure/Repositories/TasksGroupsRepo.cs
@@ -2,6 +2,7 @@
 using EmployeesTasksTracker.TasksGroupsService.Core.Models;
 using EmployeesTasksTracker.TasksGroupsService.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Shared.Exceptions;
 
 namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Repositories
 {
@@ -23,7 +24,7 @@ namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Repositories
 
             if (await _context.TasksGroups.AnyAsync(tg => tg.Name == tasksGroup.Name))
             {
-                throw new Exception("Such tasks group already exists");
+                throw new DomainException("Such tasks group already exists");
             }
 
             await _context.TasksGroups.AddAsync(tasksGroup, token);
@@ -33,19 +34,11 @@ namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Repositories
 
         public async Task<bool> DeleteAsync(Guid id, CancellationToken token = default)
         {
-            try
-            {
                 var taskGroupToDelete = await GetByIdAsync(id, token);
 
                 _context.TasksGroups.Remove(taskGroupToDelete);
                 await _context.SaveChangesAsync(token);
                 return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Could not delete tasks group with the given id {id} : {ex.Message}");
-                return false;
-            }
         }
 
         public async Task<IEnumerable<TasksGroup>> GetAllAsync(CancellationToken token = default)
@@ -68,7 +61,7 @@ namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Repositories
 
             if (taskGroupToFind == null)
             {
-                throw new Exception($"Tasks group with id: {id} not found!");
+                throw new DomainException($"Tasks group with id: {id} not found!");
             }
 
             return taskGroupToFind;
@@ -76,18 +69,11 @@ namespace EmployeesTasksTracker.TasksGroupsService.Infrastructure.Repositories
 
         public async Task<TasksGroup> UpdateAsync(TasksGroup taskGroup, CancellationToken token = default)
         {
-            try
-            {
                 var existingTaskGroup = await GetByIdAsync(taskGroup.Id, token);
 
                 _context.Entry(existingTaskGroup).CurrentValues.SetValues(taskGroup);
                 await _context.SaveChangesAsync(token);
                 return taskGroup;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Could not update tasks group named: {taskGroup.Name}", ex);
-            }
         }
     }
 }
