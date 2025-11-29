ed4a3f9fc54e92abdde6e390a775690ad8d60245
dasdsdas sdaasd <skorliak@mail.ru>
2025-11-29 12:51:00 +0400

    Added messages for history service to store information of task being created or deleted.

---
+++ Common/Shared/Messages/TaskCreated.cs
@@ -0,0 +1,6 @@
+﻿using Shared.DTOs;
+
+namespace Shared.Messages
+{
+    public record TaskCreated(Guid TaskId, TaskForReportDTO CreatedTask, DateTime CreatedAt);
+}
---
+++ Common/Shared/Messages/TaskDeleted.cs
@@ -0,0 +1,7 @@
+﻿using Shared.DTOs;
+using System.Threading.Tasks;
+
+namespace Shared.Messages
+{
+    public record TaskDeleted(Guid DeletedTaskId, DateTime DeletedAt);
+}
