51c75f1dd70af946f58a8c4b28511f1307991c59
dasdsdas sdaasd <skorliak@mail.ru>
2025-11-29 14:36:00 +0400

    Added generic Rasult class for handlers to use (Result pattern).

---
+++ Common/Shared/Models/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace Shared.Models
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string Error { get; }
+
+        public Result(bool isSuccess, string errorMessage)
+        {
+            IsSuccess = isSuccess;
+            Error = errorMessage;
+        }
+
+        public static Result Success() 
+        {
+            return new Result (true, string.Empty);
+        }
+
+        public static Result Failure(string errorMessage) 
+        {
+            return new Result (false, errorMessage);
+        }
+    }
+}
