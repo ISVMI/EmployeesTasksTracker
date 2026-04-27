5c4286613a7c001a1bbb218a5cd16f4e22dd79d4
dasdsdas sdaasd <skorliak@mail.ru>
2026-04-27 12:35:26 +0400

    Expanded tasks groups and projects deletion logic.

--- Services/TasksTrackerService/EmployeesTasksTracker.TasksTrackerService.Application/Handlers/Projects/DeleteProjectHandler.cs
+++ Services/TasksTrackerService/EmployeesTasksTracker.TasksTrackerService.Application/Handlers/Projects/DeleteProjectHandler.cs
@@ -1,21 +1,37 @@
 ﻿using EmployeesTasksTracker.TasksTrackerService.Application.Commands.Projects;
 using EmployeesTasksTracker.TasksTrackerService.Core.Interfaces;
 using MediatR;
+using Shared.Exceptions;
 
 namespace EmployeesTasksTracker.TasksTrackerService.Application.Handlers.Projects
 {
     public class DeleteProjectHandler : IRequestHandler<DeleteProjectCommand, bool>
     {
-        private readonly IProjectsRepo _repo;
+        private readonly IProjectsRepo _projectsRepo;
+        private readonly ITasksRepo _tasksRepo;
 
-        public DeleteProjectHandler(IProjectsRepo repo)
+        public DeleteProjectHandler(IProjectsRepo projectsRepo, ITasksRepo tasksRepo)
         {
-            _repo = repo;
+            _projectsRepo = projectsRepo;
+            _tasksRepo = tasksRepo;
         }
 
         public async Task<bool> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
         {
-            return await _repo.DeleteAsync(request.Id, cancellationToken);
+            var relatedTasks = await _tasksRepo.GetAllFilteredAsync(default, default, request.Id, cancellationToken);
+
+            if (relatedTasks.Any())
+            {
+                foreach (var task in relatedTasks)
+                {
+                    if (task.Status != Core.Enums.Status.Canceled || task.Status != Core.Enums.Status.Completed)
+                    {
+                        throw new DomainException($"Couldn't delete project! Task {task.Name} is {task.Status}!");
+                    }
+                }
+            }
+
+            return await _projectsRepo.DeleteAsync(request.Id, cancellationToken);
         }
     }
 }
